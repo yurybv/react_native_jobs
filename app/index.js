e11af260f08f88914c57588d8fa3d1ecd6fd1f7b
yurybv <yuriy.bv.yuriy@gmail.com>
2023-05-19 17:37:00 +0300

    move and import home page

--- app/index.js
+++ app/index.js
@@ -1,47 +1,5 @@
-import { useRouter } from 'expo-router';
-import { SafeAreaView, ScrollView, View } from 'react-native';
-import { COLORS, icons, SIZES, images } from '../constants';
-import Stack from 'expo-router/src/layouts/Stack';
-import { Nearbyjobs, ScreenHeaderBtn, Popularjobs, Welcome } from '../components';
-import { useState } from 'react';
+import { Redirect } from 'expo-router';
 
-const Home = () => {
-  const router = useRouter();
-  const [searchTerm, setSearchTerm] = useState('');
-
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-      <Stack.Screen
-        options={{
-          headerStyle: { backgroundColor: COLORS.lightWhite },
-          headerShadowVisible: false,
-          headerLeft: () => <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />,
-          headerRight: () => <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />,
-          headerTitle: '',
-        }}
-      />
-      <ScrollView showVerticalScrollIndicator={false}>
-        <View
-          style={{
-            flex: 1,
-            padding: SIZES.medium,
-          }}
-        >
-          <Welcome
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
-            handleClick={() => {
-              if (searchTerm) {
-                router.push(`/search/${searchTerm}`);
-              }
-            }}
-          />
-          <Popularjobs />
-          <Nearbyjobs />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-export default Home;
+export default function Index() {
+  return <Redirect href="/home" />;
+}
